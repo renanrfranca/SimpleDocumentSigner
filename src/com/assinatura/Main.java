3e6471ab8dcd41825da36310e14e91eb49d46433
Renan Rossignatti de França <renanrfranca@gmail.com>
2021-01-14 02:15:54 -0300

    Initial commit

---
+++ src/com/assinatura/Main.java
@@ -0,0 +1,14 @@
+package com.assinatura;
+
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+import java.util.Enumeration;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
